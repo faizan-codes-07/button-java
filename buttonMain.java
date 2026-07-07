97cf28e6de2b5a7cbb10b8f519a6351b1f155bb9
Faizan khan <faizank5413976@gmail.com>
2026-07-08 01:01:21 +0530

    Add files via upload

---
+++ buttonMain.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int button = sc.nextInt();
+        
+        if (button == 1) {
+                         System.out.println("hello");
+        } else if (button == 2) {
+                         System.out.println("namaste");      
+        } else if (button == 3) {
+                         System.out.println("bonjour");
+        } else if (button == 4) {
+                         System.out.println("konnichiwa");
+        } else {
+                         System.out.println("invalid button");
+        }
+    
+    }
+}
